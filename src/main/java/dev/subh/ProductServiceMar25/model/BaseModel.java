59c09302f720bec720a5c7cc3d7a4cbc4f396afd
Subhadeep Chakraborty <subhadeepc2020@gmail.com>
2025-04-20 13:24:58 +0200

    added repository classes. implemented selfProductService

--- src/main/java/dev/subh/ProductServiceMar25/model/BaseModel.java
+++ src/main/java/dev/subh/ProductServiceMar25/model/BaseModel.java
@@ -1,6 +1,8 @@
 package dev.subh.ProductServiceMar25.model;
 
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -13,6 +15,7 @@ import java.util.Date;
 @MappedSuperclass
 public class BaseModel {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     private Date createdAt;
